955dd4db456cff974b750282c1dc87768be45658
Luis Carlos Cruz Carballo <lcruzc@linkux-it.com>
2016-01-21 16:04:34 -0400

    Add errors

--- index.js
+++ index.js
@@ -4,21 +4,53 @@
 
 // Package imports
 import Document from './lib/document';
+import Schema from './lib/schema';
 
+import * as errors from './lib/errors';
 
 // Re export
 export { db, connect } from './lib/connection';
 
 
+let modelSchemas = {};
+
+
 /**
- * @description Define the collection using the schema.
+ * @description Define the collection using the schema or retrieve it.
  *
  * If schema is provided will create/update the collection class and will
  * return the class but if not provided it will only return the class.
  *
+ * ####Example:
+ *
+ *     var schema = new Schema({ name: String }, { collection: 'actor' });
+ *
+ *     // or
+ *
+ *     schema.set('collection', 'actor');
+ *
+ *     // or
+ *
+ *     var collectionName = 'actor'
+ *     var M = palta.model('Actor', schema, collectionName)
+ *
  * @param {string} name The name that will be used as reference.
- * @param {!Schema} schema The schema used to create the collection.
+ * @param {Schema} [schema] The schema used to create the collection.
+ * @param {String} [collection] name (optional, induced from model name)
  */
-export function collection(name, schema) {
+export function model(name, schema, collection) {
 
+  if (schema && !(schema instanceof Schema)) {
+    schema = new Schema(schema);
+  }
+
+  if (!modelSchemas[name]) {
+    if (schema) {
+      modelSchemas[name] = schema;
+      applyPlugins(schema);
+    } else {
+      throw new errors.MissingSchemaError(name);
+    }
+  }
 }
+
